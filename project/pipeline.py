4b74a5503f025a92da10e9ec5d01e926bbbcba81
rebel47 <aayyaazz123@gmail.com>
2024-12-08 19:31:34 +0100

    ci-pipeline

--- project/pipeline.py
+++ project/pipeline.py
@@ -10,8 +10,7 @@ datasets = {
     "quarterly_gdp": "https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1320&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=GDPC1&scale=left&cosd=2014-07-01&coed=2024-07-01&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Quarterly&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2024-11-08&revision_date=2024-11-08&nd=1947-01-01"
 }
 
-data ='../data'
-
+data = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data'))
 # Downloading each dataset (Annually and Quarterly) in csv format
 dataframe = {}
 for name, url in datasets.items():
